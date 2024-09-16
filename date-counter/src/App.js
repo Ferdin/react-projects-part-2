e022170d8a7881444b172bfa52b39302e4194f26
multimatic-ferdin <fnorbert@multimatic.com>
2024-09-16 16:30:29 -0400

    Date counter finalized - learn state

--- date-counter/src/App.js
+++ date-counter/src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DateCounter />
     </div>
+  );
+}
+
+function DateCounter() {
+  const [dateCounter, setDateCounter] = useState(0);
+  const [step, setStep] = useState(1);
+
+  function nextDate() {
+    setDateCounter((s) => s + step);
+  }
+
+  function prevDate() {
+    setDateCounter((s) => s - step);
+  }
+
+  const date = new Date();
+  date.setDate(date.getDate() + dateCounter);
+
+  return (
+    <>
+      <div>
+        <button onClick={() => setStep((c) => c - 1)}>-</button>
+        <span>Steps: {step}</span>
+        <button onClick={() => setStep((c) => c + 1)}>+</button>
+      </div>
+      <div>
+        <button onClick={prevDate}>-</button>
+        <span>Date</span>
+        <button onClick={nextDate}>+</button>
+      </div>
+      <div>
+        <span>
+          {dateCounter === 0
+            ? "Today is "
+            : dateCounter > 0
+            ? `${dateCounter} days from today is `
+            : `${Math.abs(dateCounter)} days ago was `}
+        </span>
+        <span>{date.toDateString()}</span>
+      </div>
+    </>
   );
 }
 
