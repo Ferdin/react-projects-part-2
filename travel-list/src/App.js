2892e55a59720e0e3f11b99cf2168f548d20741e
multimatic-ferdin <fnorbert@multimatic.com>
2024-09-17 15:53:16 -0400

    Rendering list

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,3 +1,9 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
+];
+
 export default function App() {
   return (
     <div className="App">
@@ -20,7 +26,26 @@ function Form() {
   );
 }
 function PackingList() {
-  return <div className="list">TEST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.description} {item.quantity}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 function Stats() {
   return (
